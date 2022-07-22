79793e5684c411be47eff6a220553c866f34e687
christopher020 <uchennachris020@gmail.com>
2022-07-22 03:29:51 +0100

    commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -149,3 +149,8 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
